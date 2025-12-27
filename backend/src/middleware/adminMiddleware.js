a6793dbe3d607dfd4a0302eeec62397553f3c5fb
Pratibha Sharma <pratibhaa625@gmail.com>
2025-12-27 21:19:11 +0545

    admin-based

---
+++ backend/src/middleware/adminMiddleware.js
@@ -0,0 +1,9 @@
+const admin = (req, res, next) => {
+    if (req.user && req.user.role === "admin") {
+        next(); // user is admin, allow access
+    } else {
+        res.status(403).json({ message: "Forbidden: Admins only" });
+    }
+};
+
+module.exports = { admin };
--- backend/src/routes/packageRoutes.js
+++ backend/src/routes/packageRoutes.js
@@ -1,15 +1,15 @@
 const express =require("express");
 const { getAllPackages, createPackage, updatePackage, deletePackage } = require("../controllers/packageController");
 const { protect } = require("../middleware/authMiddleware");
-
+const { admin } = require("../middleware/adminMiddleware");
 const router=express.Router();
 
 // Private(Admin)
-router.post("/",protect, createPackage);
+router.post("/",protect,admin, createPackage);
 // Public
-router.get("/", getAllPackages);
+router.get("/",protect,admin, getAllPackages);
 // Protected
-router.put("/:id",protect ,updatePackage);
+router.put("/:id",protect ,admin ,updatePackage);
 // Protected
 router.delete("/:id", protect, deletePackage);
 
